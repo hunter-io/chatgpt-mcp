dbd0afd36c22fd9c814af19208f5959feb1e0313
antoinefink <2157992+antoinefink@users.noreply.github.com>
2026-05-14 14:21:10 +0000

    Sync from hunter-io/hunter@5ed69ae

--- global.d.ts
+++ global.d.ts
@@ -8,3 +8,6 @@ declare module '*.css?raw' {
   const content: string;
   export default content;
 }
+
+// TS6 enables noUncheckedSideEffectImports by default; allow side-effect CSS imports
+declare module '*.css';
